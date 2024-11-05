471638dae3705001ccca7e9a268f6982a723321a
Bobby Roe <bobby.roe@unifyconsulting.com>
2024-11-04 21:01:28 -0800

    add all the things

---
+++ index.js
@@ -0,0 +1,62 @@
+import * as THREE from "three";
+import { OrbitControls } from 'jsm/controls/OrbitControls.js';
+import getStarfield from "./src/getStarfield.js";
+import { drawThreeGeo } from "./src/threeGeoJSON.js";
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+const scene = new THREE.Scene();
+scene.fog = new THREE.FogExp2(0x000000, 0.3);
+const camera = new THREE.PerspectiveCamera(75, w / h, 1, 100);
+camera.position.z = 5;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+const geometry = new THREE.SphereGeometry(2);
+const lineMat = new THREE.LineBasicMaterial({ 
+  color: 0xffffff,
+  transparent: true,
+  opacity: 0.4, 
+});
+const edges = new THREE.EdgesGeometry(geometry, 1);
+const line = new THREE.LineSegments(edges, lineMat);
+scene.add(line);
+
+const stars = getStarfield({ numStars: 1000, fog: false });
+scene.add(stars);
+
+// check here for more datasets ...
+// https://github.com/martynafford/natural-earth-geojson
+// non-geojson datasets: https://www.naturalearthdata.com/downloads/
+fetch('./geojson/ne_110m_land.json')
+  .then(response => response.text())
+  .then(text => {
+    const data = JSON.parse(text);
+    const countries = drawThreeGeo({
+      json: data,
+      radius: 2,
+      materialOptions: {
+        color: 0x80FF80,
+      },
+    });
+    scene.add(countries);
+  });
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  controls.update();
+}
+
+animate();
+
+function handleWindowResize () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', handleWindowResize, false);
